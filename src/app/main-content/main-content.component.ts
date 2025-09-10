4fc0738927defdcb2a507288f7ccb08615415d9e
Nils Alberding-Schulz <nilsalberding@web.de>
2025-09-10 21:04:10 +0200

    add empty main-content component and hero-section-component

---
+++ src/app/main-content/main-content.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { HeroSectionComponent } from './hero-section/hero-section.component';
+
+@Component({
+  selector: 'app-main-content',
+  imports: [HeroSectionComponent],
+  templateUrl: './main-content.component.html',
+  styleUrl: './main-content.component.scss'
+})
+export class MainContentComponent {
+
+}
