cb6fc86ca7bc239442b6607559e6f257988f6e87
Nils Alberding-Schulz <nilsalberding@web.de>
2025-09-11 12:44:17 +0200

    styling for hero-section

--- src/app/shared/component/header/header.component.ts
+++ src/app/shared/component/header/header.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { TranslateDirective, TranslatePipe, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [TranslatePipe],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
 
+  private translate = inject(TranslateService);
+
+  useLanguage(language: string): void {
+    this.translate.use(language);
+  }
 }
