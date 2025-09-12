b8f1dabadc8fa00dcf5f7f17d095f1ab03055b5b
Nils Alberding-Schulz <nilsalberding@web.de>
2025-09-12 11:01:29 +0200

    set active buttons

--- src/app/shared/component/header/header.component.ts
+++ src/app/shared/component/header/header.component.ts
@@ -1,17 +1,32 @@
+import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { TranslateDirective, TranslatePipe, TranslateService } from '@ngx-translate/core';
 
+
 @Component({
   selector: 'app-header',
-  imports: [TranslatePipe],
+  imports: [TranslatePipe, CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
 
+  btnEn = true;
+  btnDe = false;
+
   private translate = inject(TranslateService);
 
   useLanguage(language: string): void {
     this.translate.use(language);
+  }
+
+  setDe(): void {
+    this.btnDe = true;
+    this.btnEn = false;
+  }
+
+  setEn(): void {
+    this.btnDe = false;
+    this.btnEn = true;
   }
 }
