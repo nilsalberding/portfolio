12bd430d6e2dfdeb377a8407dfcd1d9dd30da51e
Nils Alberding-Schulz <nilsalberding@web.de>
2025-09-16 14:57:34 +0200

    add service component with data of projects

---
+++ src/app/services/projectsdata.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProjectsdataService {
+
+  constructor() { }
+
+  projects = [
+    {
+      name: "Pokedex",
+      img: 'assets/img/pokedex.png',
+      duration: 'projects.pokedex.duration',
+      about: 'projects.pokedex.about',
+      organized: 'projects.pokedex.organized',
+      learning: 'projects.pokedex.learning'
+    },
+    {
+      name: "El Pollo Loco",
+      img: 'assets/img/elpolloloco.png',
+      duration: 'projects.game.duration',
+      about: 'projects.game.about',
+      organized: 'projects.game.organized',
+      learning: 'projects.game.learning'
+    },
+    {
+      name: "Join",
+      img: 'assets/img/join.png',
+      duration: 'projects.join.duration',
+      about: 'projects.join.about',
+      organized: 'projects.join.organized',
+      learning: 'projects.join.learning'
+    }
+  ]
+}
