4b4e35c90c2c4ce32956bbd592608d21b71a10ab
Nils Alberding-Schulz <nilsalberding@web.de>
2025-09-22 20:34:21 +0200

    add legal-notice

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { MainContentComponent } from './main-content/main-content.component';
 import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
+import { LegalNoticeComponent } from './legal-notice/legal-notice.component';
 
 export const routes: Routes = [
     {path: '', component: MainContentComponent},
     {path: 'privacy', component: PrivacyPolicyComponent},
+    {path: 'legal-notice', component: LegalNoticeComponent},
 
 ];
---
+++ src/app/legal-notice/legal-notice.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from '../shared/component/header/header.component';
+
+@Component({
+  selector: 'app-legal-notice',
+  imports: [HeaderComponent],
+  templateUrl: './legal-notice.component.html',
+  styleUrl: './legal-notice.component.scss'
+})
+export class LegalNoticeComponent {
+
+}
