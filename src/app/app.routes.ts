02810217025c85a51b4369eccf2b5657ba0ae7e9
Nils Alberding-Schulz <nilsalberding@web.de>
2025-09-22 20:27:16 +0200

    add privacy-policy

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { MainContentComponent } from './main-content/main-content.component';
+import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
 
 export const routes: Routes = [
-    {path: '', component: MainContentComponent}
+    {path: '', component: MainContentComponent},
+    {path: 'privacy', component: PrivacyPolicyComponent},
+
 ];
---
+++ src/app/privacy-policy/privacy-policy.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from '../shared/component/header/header.component';
+
+@Component({
+  selector: 'app-privacy-policy',
+  imports: [HeaderComponent],
+  templateUrl: './privacy-policy.component.html',
+  styleUrl: './privacy-policy.component.scss'
+})
+export class PrivacyPolicyComponent {
+
+}
