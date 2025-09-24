ed4ce089a1b26c7d78af66dc9a549895b3e3c8fb
Nils Alberding-Schulz <nilsalberding@web.de>
2025-09-24 15:28:25 +0200

    bugfix

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -15,7 +15,7 @@ export const appConfig: ApplicationConfig = {
       lang: 'en',
       fallbackLang: 'en',
       loader: provideTranslateHttpLoader({
-        prefix: '/i18n/',
+        prefix: './i18n/',
         suffix: '.json'
       })
     }),
