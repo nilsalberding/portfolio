2c9900dbf9e791d976ab92f3ffd1a830fb704964
Nils Alberding-Schulz <nilsalberding@web.de>
2025-11-11 20:04:45 +0100

    add translate-pipe

--- src/app/legal-notice/legal-notice.component.ts
+++ src/app/legal-notice/legal-notice.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../shared/component/header/header.component';
 import { HeaderMobileComponent } from '../shared/component/header-mobile/header-mobile.component';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-legal-notice',
-  imports: [HeaderComponent, HeaderMobileComponent],
+  imports: [HeaderComponent, HeaderMobileComponent, TranslatePipe],
   templateUrl: './legal-notice.component.html',
   styleUrl: './legal-notice.component.scss'
 })
