02fb6308442951edd131b96dc6b40b295a0a81e2
Nils Alberding-Schulz <nilsalberding@web.de>
2025-11-11 15:53:23 +0100

    save translation in local storage

--- src/app/shared/component/header-mobile/header-mobile.component.ts
+++ src/app/shared/component/header-mobile/header-mobile.component.ts
@@ -15,28 +15,60 @@ export class HeaderMobileComponent {
 
   burgerMenuOpen = false;
 
+  translationOn = false;
+
 
   private translate = inject(TranslateService);
 
   useLanguage(language: string): void {
     this.translate.use(language);
+    if(language == "de"){
+      this.setDe();
+    }else if(language == "en"){
+      this.setEn();
+    }
   }
 
   setDe(): void {
     this.lng.lngBtns.de = true;
     this.lng.lngBtns.en = false;
+    this.translationOn = true;
+    this.saveInLocalStorage();
   }
 
   setEn(): void {
     this.lng.lngBtns.de = false;
     this.lng.lngBtns.en = true;
+    this.translationOn = false;
+    this.saveInLocalStorage();
   }
 
-  showBurgerMenu(){
+  showBurgerMenu() {
     this.burgerMenuOpen = true;
   }
 
-  closeBurgerMenu(){
+  closeBurgerMenu() {
     this.burgerMenuOpen = false;
+  }
+
+  saveInLocalStorage() {
+    const json = JSON.stringify(this.translationOn);
+    localStorage.setItem('lng', json)
+  }
+
+  loadFromLocalStorage() {
+    const item = localStorage.getItem('lng');
+    if (item) {
+      this.translationOn = JSON.parse(item);
+    }
+  }
+
+  ngOnInit(){
+    this.loadFromLocalStorage();
+    if(this.translationOn){
+      this.useLanguage("de");
+    }else{
+      this.useLanguage("en");
+    }
   }
 }
--- src/app/shared/component/header/header.component.ts
+++ src/app/shared/component/header/header.component.ts
@@ -14,23 +14,52 @@ export class HeaderComponent {
 
   lng = inject(LanguageBtnsService);
 
-  btnEn = true;
-  btnDe = false;
+  translationOn = false;
 
   private translate = inject(TranslateService);
 
   useLanguage(language: string): void {
     this.translate.use(language);
+    if (language == "de") {
+      this.setDe();
+    } else if (language == "en") {
+      this.setEn();
+    }
   }
 
   setDe(): void {
     this.lng.lngBtns.de = true;
     this.lng.lngBtns.en = false;
+    this.translationOn = true;
+    this.saveInLocalStorage();
   }
 
   setEn(): void {
     this.lng.lngBtns.de = false;
     this.lng.lngBtns.en = true;
+    this.translationOn = false;
+    this.saveInLocalStorage();
+  }
+
+  saveInLocalStorage() {
+    const json = JSON.stringify(this.translationOn);
+    localStorage.setItem('lng', json)
+  }
+
+  loadFromLocalStorage() {
+    const item = localStorage.getItem('lng');
+    if (item) {
+      this.translationOn = JSON.parse(item);
+    }
+  }
+
+  ngOnInit() {
+    this.loadFromLocalStorage();
+    if (this.translationOn) {
+      this.useLanguage("de");
+    } else {
+      this.useLanguage("en");
+    }
   }
 
 }
