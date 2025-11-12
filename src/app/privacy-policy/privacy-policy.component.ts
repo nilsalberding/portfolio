f5d18da15f69bee6409051f86033e2124fd86aaa
Nils Alberding-Schulz <nilsalberding@web.de>
2025-11-12 11:19:57 +0100

    add translation to privacy-policy

--- src/app/privacy-policy/privacy-policy.component.ts
+++ src/app/privacy-policy/privacy-policy.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../shared/component/header/header.component';
 import { HeaderMobileComponent } from '../shared/component/header-mobile/header-mobile.component';
+import { TranslatePipe } from '@ngx-translate/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-privacy-policy',
-  imports: [HeaderComponent, HeaderMobileComponent],
+  imports: [HeaderComponent, HeaderMobileComponent, TranslatePipe, CommonModule],
   templateUrl: './privacy-policy.component.html',
   styleUrl: './privacy-policy.component.scss'
 })
